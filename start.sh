9f8e38247e6cb27a4aadb334831483b3da62ac03
Cristinarte <cristinarh.nrh@gmail.com>
2025-04-15 12:04:56 +0200

    Agrego storage:link automático al start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# Crear el enlace simbólico si no existe
+php artisan storage:link || true
+
+# Cachear configuración
+php artisan config:cache
+
+# Iniciar PHP-FPM y Nginx
 php-fpm -D
 nginx -g "daemon off;"
